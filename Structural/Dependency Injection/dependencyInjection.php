63269a3b6a8a25a554188e5552cb857c38c8a59a
Don Li <donli1210@gmail.com>
2018-07-11 13:39:53 -0500

    proxy and DI

---
+++ Structural/Dependency Injection/dependencyInjection.php
@@ -0,0 +1,114 @@
+<?php
+// To implement a loosely coupled architecture in order to get better testable, 
+// maintainable and extendable code.
+
+// DatabaseConfiguration.php
+
+namespace DesignPatterns\Structural\DependencyInjection;
+
+class DatabaseConfiguration
+{
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var int
+     */
+    private $port;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    public function __construct(string $host, int $port, string $username, string $password)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    public function getPort(): int
+    {
+        return $this->port;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
+
+// DatabaseConnection.php
+
+namespace DesignPatterns\Structural\DependencyInjection;
+
+class DatabaseConnection
+{
+    /**
+     * @var DatabaseConfiguration
+     */
+    private $configuration;
+
+    /**
+     * @param DatabaseConfiguration $config
+     */
+    public function __construct(DatabaseConfiguration $config)
+    {
+        $this->configuration = $config;
+    }
+
+    public function getDsn(): string
+    {
+        // this is just for the sake of demonstration, not a real DSN
+        // notice that only the injected config is used here, so there is
+        // a real separation of concerns here
+
+        return sprintf(
+            '%s:%s@%s:%d',
+            $this->configuration->getUsername(),
+            $this->configuration->getPassword(),
+            $this->configuration->getHost(),
+            $this->configuration->getPort()
+        );
+    }
+}
+
+// Tests/DependencyInjectionTest.php
+
+namespace DesignPatterns\Structural\DependencyInjection\Tests;
+
+use DesignPatterns\Structural\DependencyInjection\DatabaseConfiguration;
+use DesignPatterns\Structural\DependencyInjection\DatabaseConnection;
+use PHPUnit\Framework\TestCase;
+
+class DependencyInjectionTest extends TestCase
+{
+    public function testDependencyInjection()
+    {
+        $config = new DatabaseConfiguration('localhost', 3306, 'domnikl', '1234');
+        // DatabaseConfiguration is injected into DatabaseConnection
+        $connection = new DatabaseConnection($config);
+
+        $this->assertEquals('domnikl:1234@localhost:3306', $connection->getDsn());
+    }
+}
+
